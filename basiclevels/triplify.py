ecc17a9241b048f8c4d3c34f488f35d530374930
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2019-12-16 15:15:14 +0100

    Use lemma from new input format to get capitalisation correct. Now all synset URLs generated from the basiclevel data a real synset URLs also present in the WN 3.0 RDF dataset

--- basiclevels/triplify.py
+++ basiclevels/triplify.py
@@ -3,15 +3,16 @@ import re
 
 def main():
     basics_file = open ("basiclevels.txt", "r")
-    p = re.compile("(\"Synset)\('(.*)'\)")
+    p = re.compile("(Synset)\('(.*)'\)\t(.*)")
     n = re.compile("\.n\.")
     print "@prefix wn30:  <http://purl.org/vocabularies/princeton/wn30/> ."
     print "@prefix cgraph: <http://purl.org/vocabularies/cgraph/> ."
     for line in basics_file:
-        (synset, sid) = p.match(line).groups()
+        (synset, sid, lemma) = p.match(line).groups()
         (l, ns) = n.split(sid)
-        normalized1 = l.replace('\'', '_')
-        normalized = normalized1.replace('/', '_')
+        normalized1 = lemma.replace('\'', '_')
+        normalized2 = normalized1.replace('/', '_')
+        normalized  = normalized2.replace('.', '_')
         nn = int(ns)
         print("wn30:synset-%s-noun-%d a cgraph:BasicLevelSynset ." % (normalized,nn))
 
